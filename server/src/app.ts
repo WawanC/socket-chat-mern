e318822094ce85e64c7380d85c4f00327e581e99
WawanC <kurniawancristianto@gmail.com>
2021-10-13 13:46:12 +0700

    Add CORS Handler to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import corsHandler from "./utils/cors";
 
 const app = express();
+
+app.use(corsHandler);
 
 app.listen(3000, () => {
   console.log("Hello from Express");
---
+++ server/src/utils/cors.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from "express";
+
+const corsHandler: RequestHandler = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+};
+
+export default corsHandler;
