468ee8671b7d02f14c554061ddbc99337e7a1048
WawanC <kurniawancristianto@gmail.com>
2021-10-13 14:09:06 +0700

    Add Socket.IO and handling new connection between client and server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,8 @@
+import { io } from "socket.io-client";
+
 const App = () => {
+  const socket = io("http://localhost:8080");
+
   return <div className="text-red-500">Hello World</div>;
 };
 
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import http from "http";
 import corsHandler from "./utils/cors";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 
 app.use(corsHandler);
 
-app.listen(3000, () => {
-  console.log("Hello from Express");
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
 });
+
+io.on("connection", (socket: Socket) => {
+  console.log("New Connection");
+});
+
+httpServer.listen(8080);
