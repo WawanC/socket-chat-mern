1f46278d4578822412f665c122d0575904511b9d
WawanC <kurniawancristianto@gmail.com>
2021-10-13 15:38:04 +0700

    Add auto scroll to bottom in chatbox

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { io } from "socket.io-client";
 import ChatInput from "./components/ChatInput";
 import ChatBox from "./components/ChatBox";
@@ -6,6 +6,7 @@ import ChatBox from "./components/ChatBox";
 const App = () => {
   // const socket = io("http://localhost:8080");
   const [chatList, setChatList] = useState(["Hola !", "Selamat Pagi"]);
+  const chatBoxRef = useRef<HTMLDivElement>(null);
 
   const addChatList = (chatText: string) => {
     setChatList((prevState) => {
@@ -15,9 +16,13 @@ const App = () => {
     });
   };
 
+  useEffect(() => {
+    chatBoxRef.current?.scrollTo(0, chatBoxRef.current.scrollHeight);
+  }, [chatList]);
+
   return (
     <div className="w-full h-screen bg-gray-300 flex flex-col items-center py-10">
-      <ChatBox chatList={chatList} />
+      <ChatBox chatList={chatList} chatBoxRef={chatBoxRef} />
       <ChatInput onAddChat={addChatList} />
     </div>
   );
--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -2,11 +2,15 @@ import ChatBubble from "./ChatBubble";
 
 interface propsInterface {
   chatList: string[];
+  chatBoxRef: React.LegacyRef<HTMLDivElement>;
 }
 
 const ChatBox: React.FC<propsInterface> = (props) => {
   return (
-    <div className="bg-white p-4 border-2 border-black w-1/2 h-3/4 rounded flex flex-col gap-4 overflow-auto">
+    <div
+      ref={props.chatBoxRef}
+      className="bg-white p-4 border-2 border-black w-1/2 h-3/4 rounded flex flex-col gap-4 overflow-auto"
+    >
       {props.chatList.map((chat) => {
         return <ChatBubble text={chat} key={chat} />;
       })}
